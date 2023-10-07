8a2d3e7039d606dbb3a65fe5acaeb3cd894a9d19
Lekan <jimohridwanlekan@gmail.com>
2023-10-07 17:53:05 +0100

    Add new file

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from "./components/materialUi/materialUi.jsx";
 import Loading from "./components/loading";
 import HomePage from "./home/page.jsx";
 
+export const dynamic = "force-dynamic"; 
+
 export default function Home() {
   const [loading, setLoading] = React.useState(true);
 
